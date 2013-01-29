c23b241df9cb90fea72ea5174afea10302d0ee5d
konisshi hiroyuki <highwind_k@yahoo.co.jp>
2013-01-29 10:05:47 +0900

    ２回目
    配列の数を10個から3個に

--- CL_K07_1/CL_K07_1.c
+++ CL_K07_1/CL_K07_1.c
@@ -2,21 +2,21 @@
 /*課題名：課題07_1*/
 #include<stdio.h>
 
-int inputValue10(int*);
+int inputValue(int*);
 
 int main(void)
 {
-	int ary[10];
+	int ary[3];
 	int *p;
 	int i,cnt;
 
 	p = ary;
 
-	cnt = inputValue10(p);
+	cnt = inputValue(p);
 
 	printf("配列の値：");
 
-	for(i = 0; i < 9; i++){
+	for(i = 0; i < 2; i++){
 		printf("%d,",p[i]);
 	}
 	printf("%d\n",p[i]);
@@ -26,11 +26,11 @@ int main(void)
 	return 0;
 }
 
-int inputValue10(int *p)
+int inputValue(int *p)
 {
 	int i,cnt = 0;
 
-	for(i = 0; i < 10; i++){
+	for(i = 0; i < 3; i++){
 		printf("%d番目の要素の値を入力してください：",i+1);
 		scanf("%d",&p[i]);
 		cnt += p[i];
